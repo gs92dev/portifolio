5a06bbd5a17eb72a197db335a57de9155af33744
Guilherme dos Santos <gs92dev@gmail.com>
2023-04-22 15:29:23 -0500

    css adjusts

---
+++ main.js
@@ -0,0 +1,5 @@
+/*const header = document.getElementsByTagName("header");
+//const window = document.getElementsByTagName("window");
+
+
+header.addEventListener("scroll", (event) => {console.log(event)})*/
