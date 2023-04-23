bac5187b594f964fc2a09fec6e559cfc77f83aa6
Guilherme dos Santos <gs92dev@gmail.com>
2023-04-22 19:36:46 -0500

    js test

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 
 
 header.addEventListener("scroll", (event) => {console.log(event)})*/
+const isMobile = /Mobile/.test(navigator.userAgent);
+const header = document.getElementById("header");
+
+if (isMobile) {
+  // Change the element for mobile
+  header.style.backgroundColor = "red";
+} else {
+  // Change the element for desktop
+  header.style.backgroundColor = "blue";
+}
