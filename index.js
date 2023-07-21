959490a91094e799b38478632ab47c04a6112628
Guilherme dos Santos <gs92dev@gmail.com>
2023-07-21 18:44:38 -0500

    Finished timer

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const fourth = document.getElementById("fourth");
 const fifth = document.getElementById("fifth");
 const firstExperience = document.getElementById("firstExperience");
 const secondExperience = document.getElementById("secondExperience");
+const thirdExperience = document.getElementById("thirdExperience");
+const fourthExperience = document.getElementById("fourthExperience");
+const fifthExperience = document.getElementById("fifthExperience");
+//Array of nodes for each experience
+let experience = [first, second, third, fourth, fifth];
+let description = [firstExperience, secondExperience, thirdExperience, fourthExperience, fifthExperience];
+
 first.onmouseover = () => {
   firstExperience.classList.remove("none");
 };
@@ -21,24 +28,27 @@ second.onmouseout = () => {
   secondExperience.classList.add("none");
   secondExperience.classList.remove();
 };
-
-/*setInterval(() => {
-  first.classList.remove("emphasis");
-  firstExperience.classList.add("none");
-  second.classList.add("emphasis");
-  secondExperience.classList.add("emphasis");
-  secondExperience.classList.remove("none");
-}, 8000);*/
-let experience = [first, second, third, fourth, fifth];
+experience.forEach(element => {
+  element.onmouseover = () => {
+    element.classList.add("emphasis");
+  }
+  element.onmouseout = () => {
+    element.classList.remove("emphasis");
+  }
+});
+//Timer function for each experience with 5 seconds fo delay
 let time = 0;
 function timer() {
   if (time < experience.length) {
     experience[time].classList.add("emphasis");
+    description[time].classList.remove("none");
     if (time > 0) {
       experience[time - 1].classList.remove("emphasis");
+      description[time-1].classList.add("none");
     }
     if (time == 0) {
       experience[4].classList.remove("emphasis");
+      description[4].classList.add("none");
     }
     time++;
   } else {
@@ -49,4 +59,4 @@ setInterval(() => {
   timer();
 }, 5000);
 console.log("hahaha");
-//window.onload = timer();
+window.onload = timer();
