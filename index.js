ce45a51696ccb05739385232aa5df59f393a5663
Guilherme dos Santos <gs92dev@gmail.com>
2023-07-20 20:13:18 -0500

    timer adjusments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log("Working");
 const first = document.getElementById("first");
 const second = document.getElementById("second");
+const third = document.getElementById("third");
+const fourth = document.getElementById("fourth");
+const fifth = document.getElementById("fifth");
 const firstExperience = document.getElementById("firstExperience");
 const secondExperience = document.getElementById("secondExperience");
 first.onmouseover = () => {
@@ -26,11 +29,23 @@ second.onmouseout = () => {
   secondExperience.classList.add("emphasis");
   secondExperience.classList.remove("none");
 }, 8000);*/
-let experience =[first, second]
-for (let index = 0; index < experience.length; index++) {
-  setInterval(() => {
-    experience[index].classList.add("emphasis");
-  }, 7000);
-  
+let experience = [first, second, third, fourth, fifth];
+let time = 0;
+function timer() {
+  if (time < experience.length) {
+    experience[time].classList.add("emphasis");
+    if (time > 0) {
+      experience[time - 1].classList.remove("emphasis");
+    }
+    if (time == 0) {
+      experience[4].classList.remove("emphasis");
+    }
+    time++;
+  } else {
+    time = 0;
+  }
 }
-
+setInterval(() => {
+  timer();
+}, 5000);
+//window.onload = timer();
